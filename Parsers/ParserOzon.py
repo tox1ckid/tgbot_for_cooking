118f82fdea4f3d3e86b6cfc64aad60521d96895c
Maxim Dmitriev <72492980+tox1ckid@users.noreply.github.com>
2024-12-08 00:06:02 +0400

    Create ParserOzon.py

---
+++ Parsers/ParserOzon.py
@@ -0,0 +1,53 @@
+import time
+
+from selenium import webdriver
+from selenium_stealth import stealth
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+def init_webdriver():
+    driver = webdriver.Chrome()
+    stealth(driver, languages=["en-US", "en"], vendor="Google Inc.", platform="Win32", webgl_vendor="Intel Inc.", renderer="Intel Iris OpenGL Engine", fix_hairline=True)
+    driver.maximize_window()
+    return driver
+
+
+def get_products_links(item_name):
+    driver = init_webdriver()
+    driver.implicitly_wait(5)
+
+    driver.get(url='https://ozon.ru')
+    time.sleep(2)
+
+    find_input = driver.find_element(By.NAME, 'text')
+    find_input.clear()
+    find_input.send_keys(item_name)
+    time.sleep(2)
+    find_input.send_keys(Keys.ENTER)
+    driver.get(url=driver.current_url)
+
+    try:
+        find_links = driver.find_elements(By.CLASS_NAME, 'tile-hover-target')
+        products_urls = list(set([f'{link.get_attribute("href")}' for link in find_links]))
+        print('gotcha')
+    except:
+        print('smth wrong while parsing from OZON')
+
+    products_urls_dict = {}
+
+    for k, v in enumerate(products_urls):
+        products_urls_dict.update({k: v})
+
+    driver.close()
+    driver.quit()
+
+
+def main(item_name):
+    print('begin scaning')
+    get_products_links(item_name=item_name)
+    print('end successful')
+
+
+if __name__ == '__main__':
+    main()
