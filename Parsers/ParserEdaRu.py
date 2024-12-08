e92898ea892b81f318ca6cce082cf31661c16b36
Maxim Dmitriev <72492980+tox1ckid@users.noreply.github.com>
2024-12-08 04:07:27 +0400

    Update ParserEdaRu.py

--- Parsers/ParserEdaRu.py
+++ Parsers/ParserEdaRu.py
@@ -1,13 +1,18 @@
 import time
-
+from fake_useragent import UserAgent
 from selenium import webdriver
 from selenium_stealth import stealth
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+def get_random_chrome_user_agent():
+    user_agent = UserAgent(browsers='chrome', os='windows', platforms='pc')
+    return user_agent.random
+
 def init_webdriver():
     driver = webdriver.Chrome()
-    stealth(driver, languages=["en-US", "en"], vendor="Google Inc.", platform="Win32", webgl_vendor="Intel Inc.", renderer="Intel Iris OpenGL Engine", fix_hairline=True)
+    ua = get_random_chrome_user_agent()
+    stealth(driver, user_agent=ua, languages=["ru", "en-US", "en"], vendor="Google Inc.", platform="Win32", webgl_vendor="Intel Inc.", renderer="Intel Iris OpenGL Engine", fix_hairline=True)
     driver.maximize_window()
     return driver
 
