5db6beb7765959f3d0b7be27618dcb10cb5c4ed4
Maxim Dmitriev <72492980+tox1ckid@users.noreply.github.com>
2024-12-08 04:07:50 +0400

    Update ParserOzon.py

--- Parsers/ParserOzon.py
+++ Parsers/ParserOzon.py
@@ -1,14 +1,18 @@
 import time
-
+from fake_useragent import UserAgent
 from selenium import webdriver
 from selenium_stealth import stealth
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+def get_random_chrome_user_agent():
+    user_agent = UserAgent(browsers='chrome', os='windows', platforms='pc')
+    return user_agent.random
 
 def init_webdriver():
     driver = webdriver.Chrome()
-    stealth(driver, languages=["en-US", "en"], vendor="Google Inc.", platform="Win32", webgl_vendor="Intel Inc.", renderer="Intel Iris OpenGL Engine", fix_hairline=True)
+    ua = get_random_chrome_user_agent()
+    stealth(driver, user_agent=ua, languages=["ru", "en-US", "en"], vendor="Google Inc.", platform="Win32", webgl_vendor="Intel Inc.", renderer="Intel Iris OpenGL Engine", fix_hairline=True)
     driver.maximize_window()
     return driver
 
@@ -25,12 +29,10 @@ def get_products_links(item_name):
     find_input.send_keys(item_name)
     time.sleep(2)
     find_input.send_keys(Keys.ENTER)
-    driver.get(url=driver.current_url)
 
     try:
         find_links = driver.find_elements(By.CLASS_NAME, 'tile-hover-target')
-        products_urls = [f'{link.get_attribute("href")}' for link in find_links]
-        # print('gotcha')
+        products_urls = list([f'{link.get_attribute("href")}' for link in find_links])
     except:
         raise IOError("Something went wrong while parsing eda.ru")
 
